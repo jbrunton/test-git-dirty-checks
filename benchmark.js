3f4ede4f051ab7fd96510b412d7e5173509ae725
John Brunton <john_brunton@hotmail.co.uk>
2020-07-26 21:25:49 +0100

    benchmark commands

--- benchmark.js
+++ benchmark.js
@@ -12,9 +12,13 @@ if (process.argv.length != 5) {
   failWith('Usage: node benchmark.js path/to/repo existing-filename new-filename')
 }
 
-const repoPath = process.argv[2]
+const repoPath = path.isAbsolute(process.argv[2]) ? process.argv[2] : path.join(process.cwd(), process.argv[2])
 const existingFilePath = path.join(repoPath, process.argv[3])
 const newFilePath = path.join(repoPath, process.argv[4])
+
+console.log(`repoPath: ${repoPath}`)
+console.log(`existingFilePath:${existingFilePath}`)
+console.log(`newFilePath: ${newFilePath}`)
 
 if (!fs.existsSync(repoPath)) {
   failWith(`${repoPath} does not exist`)
@@ -40,4 +44,20 @@ process.chdir(repoPath)
 
 if (sh.exec('git status --porcelain').stdout != '') {
   failWith(`repo is dirty, aborting`)
+}
+
+const commands = [
+  'git diff --quiet HEAD',
+  'git update-index -q --refresh && git diff-index --quiet HEAD --',
+  'git status --porcelain --untracked-files=no',
+  'git status -suno'
+]
+
+for (let command of commands) {
+  sh.exec(`touch ${newFilePath}`)
+  sh.exec(`echo foobar > ${existingFilePath}`)
+  console.time(command)
+  sh.exec(command)
+  console.timeEnd(command)
+  sh.exec(`git clean`)
 }
