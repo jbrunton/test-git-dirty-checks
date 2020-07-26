6d1b28ee0934939fa6f8d28404ccdb1e71c43bd1
John Brunton <john_brunton@hotmail.co.uk>
2020-07-26 21:12:08 +0100

    add benchmark.js

---
+++ benchmark.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+const sh = require('shelljs')
+
+function failWith(message) {
+  console.log(message)
+  process.exit(1)
+}
+
+if (process.argv.length != 5) {
+  failWith('Usage: node benchmark.js path/to/repo existing-filename new-filename')
+}
+
+const repoPath = process.argv0[2]
+const existingFilePath = path.join(repoPath, process.argv0[3])
+const newFilePath = path.join(repoPath, process.argv0[4])
+
+if (!fs.existsSync(repoPath)) {
+  failWith(`${repoPath} does not exist`)
+}
+
+if (!fs.statSync(repoPath).isDirectory) {
+  failWith(`${repoPath} is not a directory`)
+}
+
+if (!fs.existsSync(path.join(repoPath, '.git'))) {
+  failWith(`${repoPath} is not a git directory`)
+}
+
+if (!fs.existsSync(existingFilePath)) {
+  failWith(`${existingFilePath} does not exist`)
+}
+
+if (fs.exists(newFilePath)) {
+  failWith(`${newFilePath} should not exist`)
+}
+
+process.chdir(repoPath)
+
+if (sh.exec('git status --porcelain').stdout != '') {
+  failWith(`repo is dirty, aborting`)
+}
